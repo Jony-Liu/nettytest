e29aec9f7dac25be6e2fad22a045f34f03993170
Jony Liu <liu_zengqiang@126.com>
2019-06-20 14:53:11 +0800

    修改 channelRead 方法

--- src/main/java/io/netty/example/discard/DiscardServerHandler.java
+++ src/main/java/io/netty/example/discard/DiscardServerHandler.java
@@ -3,9 +3,6 @@ package io.netty.example.discard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.ReferenceCountUtil;
-
-import java.io.UnsupportedEncodingException;
 
 /**
  * Handles a server-side channel.
@@ -14,18 +11,7 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        //Looking into the Received Data
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while (in.isReadable()) {
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
-        } finally {
-            ReferenceCountUtil.release(msg);
-        }
-        // Discard the received data silently.
-        //((ByteBuf) msg).release(); // (3)
+        ((ByteBuf) msg).release(); // (3)
     }
 
     @Override
