3437166caf00fd409463eb7ba7694198e3851cc4
Jony Liu <liu_zengqiang@126.com>
2019-06-20 14:57:17 +0800

    修改 channelRead 方法，打印请求信息

--- src/main/java/io/netty/example/discard/DiscardServerHandler.java
+++ src/main/java/io/netty/example/discard/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package io.netty.example.discard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
@@ -11,7 +12,15 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        ((ByteBuf) msg).release(); // (3)
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
     }
 
     @Override
